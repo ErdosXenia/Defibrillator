eb60d05dd7de72946a322ff0e9317e68e158b8bd
ErdosXenia <erdos.xenia@gmail.com>
2021-03-09 10:07:36 +0100

    Osztaly letrehozasa

--- Defibrillator/Keszulek.cs
+++ Defibrillator/Keszulek.cs
@@ -8,5 +8,70 @@ namespace Defibrillator
 {
     class Keszulek
     {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        private double latitude;
+
+        public double Latitude
+        {
+            get { return latitude; }
+        }
+
+        private double longitude;
+
+        public double Longitude
+        {
+            get { return longitude; }
+        }
+
+        private string tel;
+
+        public string Tel
+        {
+            get { return tel; }
+        }
+
+        private string address;
+
+        public string Address
+        {
+            get { return address; }
+        }
+
+        private double dist;
+
+        public double Dist
+        {
+            get { return dist; }
+            set { dist = value; }
+        }
+
+
+        public Keszulek(int id, string name, double latitude, double longitude, string tel, string address)
+        {
+            this.id = id;
+            this.name = name;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.tel = tel;
+            this.address = address;
+        }
+
+        public void DistCalc(double x, double y)
+        {
+
+        }
     }
 }
